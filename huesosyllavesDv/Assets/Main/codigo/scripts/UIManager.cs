573e9b5abd173259e3a77ca693aec2fc4876c143
Alejandro M <alejandro221app@gmail.com>
2025-09-30 15:58:36 -0500

    Tecla de esc funciona
    activa panel cuando se pausa juego, desactiva panel cuando se renauda el juego

--- huesosyllavesDv/Assets/Main/codigo/scripts/UIManager.cs
+++ huesosyllavesDv/Assets/Main/codigo/scripts/UIManager.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
     private bool isPaused = false;
+    [SerializeField]
+
+    private GameObject panel;
 
     void Update()
     {
@@ -22,14 +26,17 @@ public class UIManager : MonoBehaviour
 
     void Pause()
     {
+        panel.SetActive(true);
         Time.timeScale = 0f; // Detiene el tiempo del juego
         isPaused = true;
+
         // Aquí puedes activar tu menú de pausa si tienes uno
         // Ejemplo: pauseMenuUI.SetActive(true);
     }
 
     void ResumeGame()
     {
+        panel.SetActive(false);
         Time.timeScale = 1f; // Reanuda el tiempo del juego
         isPaused = false;
         // Aquí puedes desactivar el menú de pausa
